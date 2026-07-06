ca86ec3e568ceca449b140954cfea6710e0f4d15
santha-ashwin-psd <santhaashwin@psdigitise.com>
2026-07-06 18:37:48 +0530

    customer field fix

---
+++ zoho_books_clone/invoicing/doctype/customer/test_customer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, PS Digitise and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomer(FrappeTestCase):
+	pass
